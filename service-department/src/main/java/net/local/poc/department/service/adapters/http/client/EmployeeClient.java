07466c0511d2bb45f46ebe23192794542095cb9f
mmendespro <marcelo.mendes.gomes@gmail.com>
2023-04-11 08:31:39 -0300

    improve logs

--- service-department/src/main/java/net/local/poc/department/service/adapters/http/client/EmployeeClient.java
+++ service-department/src/main/java/net/local/poc/department/service/adapters/http/client/EmployeeClient.java
@@ -6,10 +6,12 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 
+import lombok.extern.slf4j.Slf4j;
 import net.local.poc.department.service.application.domain.dto.EmployeeDTO;
 import net.local.poc.department.service.application.ports.outgoing.LoadDepartmentEmployeesPort;
 import reactor.core.publisher.Flux;
 
+@Slf4j
 @Component
 public class EmployeeClient implements LoadDepartmentEmployeesPort {
 
@@ -24,6 +26,7 @@ public class EmployeeClient implements LoadDepartmentEmployeesPort {
 
     @Override
     public List<EmployeeDTO> load(String departmentId) {
+        log.info("get employees from {} for departmentId {}", CLIENT_URL, departmentId);
         Flux<EmployeeDTO> response = webClient.get()
                                               .uri("/department/".concat(departmentId))
                                               .retrieve()
